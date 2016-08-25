26b66e9fa1755ed2f1e8d0b1a946db7367b75c09
Julius Krah <juliuskr@stlghana.com>
2016-08-25 23:21:57 +0000

    Added and later removed embedded tomcat until a proper fix is implemented

--- src/main/java/com/juliuskrah/multipart/entity/BaseEntity.java
+++ src/main/java/com/juliuskrah/multipart/entity/BaseEntity.java
@@ -15,12 +15,16 @@
 */
 package com.juliuskrah.multipart.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public abstract class BaseEntity {
+public abstract class BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue
 	private Long id;
